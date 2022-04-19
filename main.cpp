a153198553dbb214a08aebc8ddcb72949dbaff1a
Douglas G. Macharet <doug@dcc.ufmg.br>
2022-04-19 16:12:39 -0300

    Corrigi a indentação.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int b2d(int n) {
     int temp = num;
     while(temp){
         int last = temp % 10;
-    temp = temp / 10;
-    dec += last * b;
+        temp = temp / 10;
+        dec += last * b;
         b = b * 2;
     }
-return dec;
+    return dec;
 }
 
 int main() {
